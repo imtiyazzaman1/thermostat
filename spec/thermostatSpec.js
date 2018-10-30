f9630af088afce807dde188a87c3569e46a66e9d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-30 14:13:26 +0000

    passed test for starting temp. added unit test for #up method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,10 +1,19 @@
 describe('Thermostat', function (){
-var thermostat;
-beforeEach(function (){
-  thermostat = new Thermostat();
-});
-it("should have a starting temperature of 20", function() {
-  expect(thermostat.temp).toEqual(20)
-})
+  var thermostat;
+  beforeEach(function (){
+    thermostat = new Thermostat();
+  });
 
-})
+  it("should have a starting temperature of 20", function() {
+    expect(thermostat.temp).toEqual(20);
+  });
+
+  describe('#up', function () {
+
+    it('increases the temperature by 1', function () {
+      thermostat.up();
+      expect(thermostat.temp).toEqual(21);
+    });
+
+  });
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,5 @@
+const STARTING_TEMP = 20;
+
+function Thermostat() {
+  this.temp = STARTING_TEMP;
+}
