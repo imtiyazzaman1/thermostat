49685573f86bba0adb8f465100d060f6f00ac6c1
rbbri <robin.s.johnson@gmail.com>
2018-10-31 11:40:14 +0000

    Changed to js standard

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,108 +1,96 @@
-'use strict';
+/* eslint-env jasmine */
+'use strict'
 
-describe('Thermostat', function (){
-  var thermostat;
-  beforeEach(function (){
-    thermostat = new Thermostat();
-  });
+describe('Thermostat', function () {
+  var thermostat
+  beforeEach(function () {
+    thermostat = new Thermostat()
+  })
 
-  it("should have a starting temperature of 20", function() {
-    expect(thermostat.temp).toEqual(STARTING_TEMP);
-  });
+  it('should have a starting temperature of 20', function () {
+    expect(thermostat.temp).toEqual(STARTING_TEMP)
+  })
 
-  it("should have power saving on by default", function() {
-    expect(thermostat.isPowerSaving).toBe(true);
-  });
+  it('should have power saving on by default', function () {
+    expect(thermostat.isPowerSaving).toBe(true)
+  })
 
   describe('#up', function () {
-
     it('increases the temperature by 1', function () {
-      thermostat.up();
-      expect(thermostat.temp).toEqual(21);
-    });
-
-  });
+      thermostat.up()
+      expect(thermostat.temp).toEqual(21)
+    })
+  })
 
   describe('#down', function () {
-
     it('decreases the temperature by 1', function () {
-      thermostat.down();
-      expect(thermostat.temp).toEqual(19);
-    });
-
-  });
+      thermostat.down()
+      expect(thermostat.temp).toEqual(19)
+    })
+  })
 
   describe('.minTemp', function () {
-
     it('does not lower the temp below 10', function () {
-      thermostat.temp = MINIMUM_TEMP;
-      expect(function() { thermostat.down(); } ).toThrow('Temperature cannot be lower than 10');
-    });
-  });
+      thermostat.temp = MINIMUM_TEMP
+      expect(function () { thermostat.down() }).toThrow(new Error('Temperature cannot be lower than 10'))
+    })
+  })
 
   describe('.isPowerSaving', function () {
-
     it('does not raise the temp above 25 when on', function () {
       thermostat.isPowerSaving = true
-      thermostat.temp = MAX_TEMP_PSM_ON;
-      expect(function() { thermostat.up(); } ).toThrow('Temperature cannot be higher than 25');
-    });
+      thermostat.temp = MAX_TEMP_PSM_ON
+      expect(function () { thermostat.up() }).toThrow(new Error('Temperature cannot be higher than 25'))
+    })
 
     it('does raise the temp above 25 when off', function () {
-      thermostat.isPowerSaving = false;
-      thermostat.temp = MAX_TEMP_PSM_ON;
-      thermostat.up();
-      expect(thermostat.temp).toEqual(26);
+      thermostat.isPowerSaving = false
+      thermostat.temp = MAX_TEMP_PSM_ON
+      thermostat.up()
+      expect(thermostat.temp).toEqual(26)
     })
 
     it('does not raise the temp above 32 when off', function () {
-      thermostat.isPowerSaving = false;
-      thermostat.temp = MAX_TEMP_PSM_OFF;
-      expect(function() { thermostat.up(); } ).toThrow('Temperature cannot be higher than 32');
-
+      thermostat.isPowerSaving = false
+      thermostat.temp = MAX_TEMP_PSM_OFF
+      expect(function () { thermostat.up() }).toThrow(new Error('Temperature cannot be higher than 32'))
     })
-  });
+  })
 
   describe('#reset', function () {
-
-    it('reset temperature to 20', function functionName() {
-      thermostat.temp = MAX_TEMP_PSM_ON;
-      thermostat.reset();
-      expect(thermostat.temp).toEqual(STARTING_TEMP);
+    it('reset temperature to 20', function functionName () {
+      thermostat.temp = MAX_TEMP_PSM_ON
+      thermostat.reset()
+      expect(thermostat.temp).toEqual(STARTING_TEMP)
     })
-
-  });
+  })
 
   describe('#usage', function () {
-
     it('should return low usage when temp below 18', function () {
-      thermostat.temp = 17;
+      thermostat.temp = 17
       expect(thermostat.usage()).toEqual('low-usage')
-    });
+    })
 
     it('should return medium usage when temp below 25', function () {
       expect(thermostat.usage()).toEqual('medium-usage')
-    });
+    })
 
     it('should return high usage if temp above 25', function () {
-      thermostat.temp = MAX_TEMP_PSM_ON;
+      thermostat.temp = MAX_TEMP_PSM_ON
       expect(thermostat.usage()).toEqual('high-usage')
-    });
+    })
   })
 
- describe('#switchPowerSaving', function () {
+  describe('#switchPowerSaving', function () {
+    it('switches power saving off', function () {
+      thermostat.switchPowerSaving()
+      expect(thermostat.isPowerSaving).toBe(false)
+    })
 
-   it('switches power saving off', function(){
-     thermostat.switchPowerSaving();
-     expect(thermostat.isPowerSaving).toBe(false)
-   });
-
-   it('switches power saving on', function(){
-     thermostat.isPowerSaving = false
-     thermostat.switchPowerSaving();
-     expect(thermostat.isPowerSaving).toBe(true)
-   });
-
- })
-
-});
+    it('switches power saving on', function () {
+      thermostat.isPowerSaving = false
+      thermostat.switchPowerSaving()
+      expect(thermostat.isPowerSaving).toBe(true)
+    })
+  })
+})
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,63 +1,60 @@
-const STARTING_TEMP = 20;
-const MINIMUM_TEMP = 10;
-const LOW_USAGE = 18;
-const MEDIUM_USAGE = 25;
-const MAX_TEMP_PSM_ON = 25;
-const MAX_TEMP_PSM_OFF = 32;
+const STARTING_TEMP = 20
+const MINIMUM_TEMP = 10
+const LOW_USAGE = 18
+const MEDIUM_USAGE = 25
+const MAX_TEMP_PSM_ON = 25
+const MAX_TEMP_PSM_OFF = 32
 
-function Thermostat() {
-  this.temp = STARTING_TEMP;
-  this.minTemp = MINIMUM_TEMP;
-  this.isPowerSaving = true;
+function Thermostat () {
+  this.temp = STARTING_TEMP
+  this.minTemp = MINIMUM_TEMP
+  this.isPowerSaving = true
 }
 
 Thermostat.prototype.up = function () {
-  this._checkPowerSaving();
+  this._checkPowerSaving()
   this.temp++
-};
+}
 
 Thermostat.prototype.down = function () {
-  this._checkMinTemp();
+  this._checkMinTemp()
   this.temp--
-};
+}
 
 Thermostat.prototype.reset = function () {
   this.temp = STARTING_TEMP
-};
+}
 
 Thermostat.prototype.usage = function () {
   if (this.temp < LOW_USAGE) {
-    return 'low-usage';
+    return 'low-usage'
+  } else if (this.temp < MEDIUM_USAGE) {
+    return 'medium-usage'
+  } else {
+    return 'high-usage'
   }
-  else if (this.temp < MEDIUM_USAGE) {
-    return 'medium-usage';
-  }
-  else {
-    return 'high-usage';
-  }
-};
+}
 
 Thermostat.prototype.switchPowerSaving = function () {
-  this.isPowerSaving = !this.isPowerSaving;
-};
+  this.isPowerSaving = !this.isPowerSaving
+}
 
 Thermostat.prototype._checkPowerSaving = function () {
   if (this.temp === this._maxTemp()) {
-    throw('Temperature cannot be higher than ' + this._maxTemp())
+    throw new Error('Temperature cannot be higher than ' + this._maxTemp())
   }
-};
+}
 
 Thermostat.prototype._checkMinTemp = function () {
-  if(this.temp<=this.minTemp) {
-    throw 'Temperature cannot be lower than 10';
+  if (this.temp <= this.minTemp) {
+    throw new Error('Temperature cannot be lower than 10')
   }
-};
+}
 
 Thermostat.prototype._maxTemp = function () {
-  if(this.isPowerSaving) {
-    return MAX_TEMP_PSM_ON;
+  if (this.isPowerSaving) {
+    return MAX_TEMP_PSM_ON
+  } else {
+    return MAX_TEMP_PSM_OFF
   }
-  else {
-    return MAX_TEMP_PSM_OFF;
-  }
-};
+}
